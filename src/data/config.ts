35cf1ea789fdb8678abc5c4353009004dca2cd6c
Taci <taciyalcin@gmail.com>
2025-05-28 17:44:33 +0300

    Update config.ts

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,20 +1,20 @@
 export const siteConfig = {
-  companyName: 'Titan Core',
-  siteUrl: 'https://titan-core.com',
+  companyName: 'AcilEmlakSat.com',
+  siteUrl: 'https://acilemlaksat.com',
   Socials: {
       xSocial: 'https://x.com/astrodotbuild',
       Github: 'https://github.com/withastro',
       Instagram: 'https://www.instagram.com/astrodotbuild',
       LinkedIn: 'https://www.linkedin.com/company/astrodotbuild',
-      Email: 'support@titan-core.com',
-      Phone: '+123 456 7890',
-      Location: '123 Main St, Anytown, USA',
+      Email: 'info@acilemlaksat.com',
+      Phone: '+905385244939',
+      Location: 'İkon Ofis, İsmetpaşa, Göktürk Sk. No:9 Apartmanı Daire:13, 17000 Çanakkale Merkez/Çanakkale',
   }
 };
 
 export const SEO = {
   Separator: '|',
-  SiteName: 'Titan Core',
+  SiteName: 'AcilEmlakSat.com',
   defaultDescription: 'A modern, high-performance Astro JS theme',
 };
   
